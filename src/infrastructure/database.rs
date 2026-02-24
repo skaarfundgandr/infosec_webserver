48e4715cbb37860eac5a168a0ee5a97f50a67b09
jm-eospc <mac2x.manos@gmail.com>
2026-02-24 19:49:11 +0800

    REFAC(database): Simplify database connection pool creation

--- src/infrastructure/database.rs
+++ src/infrastructure/database.rs
@@ -32,9 +32,8 @@ static DB_POOL: Lazy<Pool<AsyncMysqlConnection>> = Lazy::new(|| {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let config = AsyncDieselConnectionManager::<AsyncMysqlConnection>::new(database_url);
-    let pool = Pool::builder(config)
-        .build()
-        .expect("Failed to create database connection pool");
 
-    pool
+    Pool::builder(config)
+        .build()
+        .expect("Failed to create database connection pool")
 });
