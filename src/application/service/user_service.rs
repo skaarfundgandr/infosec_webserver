d6149102669ead10b867beffd92ef2a63da3c2b5
jm-eospc <mac2x.manos@gmail.com>
2026-02-26 11:39:22 +0800

    FEAT(user-service): Implement user creation service with error handling

--- src/application/service/user_service.rs
+++ src/application/service/user_service.rs
@@ -1,1 +1,25 @@
+use crate::application::repository::user_repo::UserRepo;
+use crate::application::security::hasher;
+use crate::domain::errors::service::UserServiceError;
+use crate::domain::models::user::NewUser;
 
+pub struct UserService;
+
+impl UserService {
+    pub async fn create_user(name: &str, password: &str) -> Result<(), UserServiceError> {
+        let password_hash = hasher::hash_password(password)
+            .await
+            .map_err(|_| UserServiceError::UserCreationError)?;
+
+        let new_user = NewUser {
+            name,
+            password_hash: &password_hash,
+        };
+
+        UserRepo::create(new_user)
+            .await
+            .map_err(|_| UserServiceError::UserCreationError)?;
+
+        Ok(())
+    }
+}
---
+++ src/domain/errors/service.rs
@@ -0,0 +1,14 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+pub enum UserServiceError {
+    UserCreationError,
+}
+
+impl Display for UserServiceError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UserServiceError::UserCreationError => write!(f, "Error creating user"),
+        }
+    }
+}
