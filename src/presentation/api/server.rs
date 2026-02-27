12ba79a64c8dc7f633f8a6f10d39c3c8e9dd64c7
jm-eospc <mac2x.manos@gmail.com>
2026-02-27 09:59:40 +0800

    FEAT(server): Implement graceful shutdown for the server using signal handling

--- src/presentation/api/server.rs
+++ src/presentation/api/server.rs
@@ -3,6 +3,9 @@ use axum::routing::get;
 use axum::{Router, serve};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tokio::signal;
+use tokio::signal::unix;
+use tokio::signal::unix::SignalKind;
 use tower_http::cors::{Any, CorsLayer};
 
 pub async fn start() {
@@ -22,6 +25,31 @@ pub async fn start() {
     println!("Attempting to start server on port 8080...");
 
     serve(listener, router)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Failed to start server!");
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        unix::signal(SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
